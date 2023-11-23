deba0c3c82c6d0e84e331cf57c3519fc3e74c788
SerhiiChynin <sergey.chinin@gmail.com>
2023-11-23 18:27:50 +0200

    fix length

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    if (message.length - 1 <= maxLength) {
+    if (message.length <= maxLength) {
         return message;
     } else {
         return message.slice(0, maxLength) + '...';
